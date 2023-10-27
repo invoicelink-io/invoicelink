b1e5ac93d4e4a664027242a6716474f193b3a284
Ratul Maharaj <ratulmaharaj@gmail.com>
2023-10-27 23:09:59 +0200

    Add theme changer hotkey

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,11 +1,25 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import { page } from '$app/stores';
+	import { shortcut } from '@svelte-put/shortcut';
 </script>
 
 <svelte:head>
 	<title>{$page.data.title ?? `invoicelink.io`}</title>
 </svelte:head>
+
+<svelte:window
+	use:shortcut={{
+		trigger: {
+			key: '†',
+			modifier: 'alt',
+			callback: () => {
+				const theme = document.documentElement.getAttribute('data-theme');
+				document.documentElement.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
+			}
+		}
+	}}
+/>
 
 <Header />
 <main class="w-full max-w-7xl p-4">
