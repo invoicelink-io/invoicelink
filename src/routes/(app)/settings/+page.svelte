78a1f3491f485a7d0ce53b2f0314b112f9e53c6d
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2023-11-30 15:20:46 +0200

    Update profile information

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,34 +1,14 @@
-<div class="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
-	<div>
-		<h2 class="text-base font-normal">Profile</h2>
-		<p class="mt-1 text-sm text-surface-500">
-			This information will be displayed publicly so be careful what you share.
-		</p>
+<h2 class="text-base font-normal">Profile</h2>
+<p class="text-surface-700-200-token mt-1 text-sm">Update your profile information</p>
 
-		<dl
-			class="mt-6 space-y-6 divide-y divide-surface-800/10 border-t border-gray-200 text-sm leading-6"
-		>
-			<div class="pt-6 sm:flex">
-				<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Full name</dt>
-				<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-					<div class="">Tom Cook</div>
-					<button type="button" class="font-semibold text-primary-500">Update</button>
-				</dd>
+<ul role="list" class="settings-list">
+	<li>
+		<div class="flex w-full flex-col items-end gap-4">
+			<div class="flex w-full items-center justify-between gap-x-2">
+				<div class="capitalize">Full Name</div>
+				<input class="capitalize" />
 			</div>
-			<div class="pt-6 sm:flex">
-				<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Email address</dt>
-				<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-					<div class="">tom.cook@example.com</div>
-					<button type="button" class="font-semibold text-primary-500">Update</button>
-				</dd>
-			</div>
-			<div class="pt-6 sm:flex">
-				<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Title</dt>
-				<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-					<div class="">Human Resources Manager</div>
-					<button type="button" class="font-semibold text-primary-500">Update</button>
-				</dd>
-			</div>
-		</dl>
-	</div>
-</div>
+			<a href="/settings/profile/edit" class="variant-soft-surface btn btn-sm">Save</a>
+		</div>
+	</li>
+</ul>
