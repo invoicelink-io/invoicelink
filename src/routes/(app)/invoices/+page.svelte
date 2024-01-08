8c7e02f49083bba7217c0390994ada1eda14b53f
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-01-08 02:32:05 +0200

    style: :lipstick: Left align cards

--- src/routes/(app)/invoices/+page.svelte
+++ src/routes/(app)/invoices/+page.svelte
@@ -9,15 +9,15 @@
 
 <div class="grid w-full gap-4 sm:grid-cols-3">
 	<a class="card-primary gap-2 p-4" href="/invoices/management">
-		<h6 class="h6 w-full text-center sm:text-left">Invoices</h6>
-		<p class="w-full text-center text-sm font-light sm:text-left">
+		<h6 class="h6 w-full text-left">Invoices</h6>
+		<p class="w-full text-left text-sm font-light">
 			Create and manage your recent invoices. View their status, download copies or resend them to
 			clients.
 		</p>
 	</a>
 	<a class="card-primary gap-2 p-4" href="/invoices/template">
-		<h6 class="h6 w-full text-center sm:text-left">Templates</h6>
-		<p class="w-full text-center text-sm font-light sm:text-left">
+		<h6 class="h6 w-full text-left">Templates</h6>
+		<p class="w-full text-left text-sm font-light">
 			Customise your invoices by adding a logo, changing colours and layouts etc.
 		</p>
 	</a>
