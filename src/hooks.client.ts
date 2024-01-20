b953325f77f24548482dcddecd9b0ee86614d0aa
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-01-20 13:20:19 +0200

    chore: :fire: Remove sentry session replay

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,21 +1,10 @@
-import { handleErrorWithSentry, Replay } from '@sentry/sveltekit';
+import { handleErrorWithSentry } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
-import { dev } from '$app/environment';
 
 Sentry.init({
 	dsn: 'https://83a0ee55052968dcb8b31d9fea069608@o4506575806398464.ingest.sentry.io/4506575811903488',
-	tracesSampleRate: 1.0,
-
-	// This sets the sample rate to be 10%. You may want this to be 100% while
-	// in development and sample at a lower rate in production
-	replaysSessionSampleRate: dev ? 1 : 0.1,
-
-	// If the entire session is not sampled, use the below sample rate to sample
-	// sessions when an error occurs.
-	replaysOnErrorSampleRate: 1.0,
-
-	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [new Replay()]
+	tracesSampleRate: 1.0
+	
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
