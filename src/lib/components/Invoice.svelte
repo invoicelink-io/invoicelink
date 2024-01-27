e466674b55de05d5b2a6c64229bd902e526f0352
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-01-27 08:45:50 +0200

    fix: :bug: Show correct issue date on invoice

--- src/lib/components/Invoice.svelte
+++ src/lib/components/Invoice.svelte
@@ -50,7 +50,7 @@
 		</div>
 		<Spacer divider={'hidden'} spacing={styles.baseSpacing} color={styles.baseDividerColor} />
 		<div class="grid grid-cols-1 leading-6">
-			<IssueDate align={styles.issueDateAlign} date={new Date()} />
+			<IssueDate align={styles.issueDateAlign} date={data.issueDate} />
 			<Spacer
 				divider={styles.baseDivider}
 				spacing={styles.baseSpacing}
