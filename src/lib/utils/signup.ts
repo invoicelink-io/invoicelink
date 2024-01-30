542c2f9761e8d1a310e5228875b98599d9d2dc5c
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-01-30 23:59:48 +0200

    feat: :sparkles: Switch to sendgrid

--- src/lib/utils/signup.ts
+++ src/lib/utils/signup.ts
@@ -1,21 +1,36 @@
+import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
-const { CONVERTKIT_API_SECRET } = env;
+const { SENDGRID_API_KEY, SENDGRID_CONTACT_LIST_ID } = env;
+import client from '@sendgrid/client';
+
+client.setApiKey(SENDGRID_API_KEY as string);
 
 export const addUserToMailingList = async (user: { email: string; name: string }) => {
-	const res = await fetch('https://api.convertkit.com/v3/tags/4475346/subscribe', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			api_secret: CONVERTKIT_API_SECRET,
-			email: user.email,
-			first_name: user.name?.split(' ')[0]
-		})
-	})
-		.then((res) => res.json())
-		.catch((err) => console.error(err));
+	if (!dev) {
+		// create a new sendgrid contact
+		const data = {
+			list_ids: [SENDGRID_CONTACT_LIST_ID],
+			contacts: [
+				{
+					email: user.email,
+					first_name: user.name
+				}
+			]
+		};
 
-	console.log(res);
-	return res;
+		await client
+			.request({
+				url: `/v3/marketing/contacts`,
+				method: 'PUT' as const,
+				body: data
+			})
+			.then(([response]) => {
+				console.log(response.statusCode, response.body);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	} else {
+		console.log(`DEV: We would only create a contact for ${user.email} in PROD`);
+	}
 };
