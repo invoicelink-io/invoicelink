e86cf828d0074b436ccf1a92f77ff3b6c8449fd9
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-02-02 22:40:30 +0200

    style: :lipstick: Update totals leading property

--- src/lib/components/invoice/Total.svelte
+++ src/lib/components/invoice/Total.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <div id="table-footer" class="grid grid-cols-3 sm:grid-cols-4">
-	<div class="col-span-2 flex flex-col items-end gap-y-2 sm:col-span-3">
+	<div class="col-span-2 flex flex-col items-end leading-6 sm:col-span-3">
 		<span class="font-normal">Subtotal</span>
 		<span class="font-normal">{invoiceType === 'VAT Invoice' ? `VAT` : `Tax`}</span>
 		<span class="font-semibold">Total</span>
 	</div>
-	<div class="flex flex-col gap-y-2">
+	<div class="flex flex-col leading-6">
 		<span class="truncate text-right tabular-nums"
 			>{data?.subtotal.toLocaleString('en-ZA', {
 				style: 'currency',
