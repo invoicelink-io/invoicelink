5d96714a1092b046563879f11f956aca1be02090
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-02-24 09:49:50 +0200

    feat: :sparkles: Expose changelog over api

---
+++ src/routes/(app)/api/changelog/+server.ts
@@ -0,0 +1,10 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { json } from '@sveltejs/kit';
+import fs from 'fs';
+
+
+export async function GET() {
+	// read in the contents of the changelog file
+	const changelog = fs.readFileSync('changelog.md', 'utf8');
+	return json(changelog);
+}
