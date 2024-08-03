bb252b21945863cae59705a876446c3d12259bbd
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-08-03 12:46:43 +0200

    fix: :bug: Add view transition name to shape shift

--- src/lib/components/ShapeShift.svelte
+++ src/lib/components/ShapeShift.svelte
@@ -1,4 +1,7 @@
-<div class="absolute inset-x-0 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
+<div
+	class="absolute inset-x-0 transform-gpu overflow-hidden blur-3xl [view-transition-name:shape-shift]"
+	aria-hidden="true"
+>
 	<div
 		id="hero-gradient-background"
 		class="animate-clip-path relative bottom-0 left-0 right-0 top-0 mx-auto h-screen w-screen bg-gradient-to-tr from-secondary via-accent to-success opacity-40"
